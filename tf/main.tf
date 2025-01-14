9601471eddc7e786937da1580adb4f0d161cafac
gespel <heimbrodt@uni-potsdam.de>
2025-01-14 14:38:09 +0100

    added module vars

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,11 @@
-module "k8s" {
+/*module "k8s" {
   source = "git::https://github.com/gespel/gprefarchinfra.git//tf/k8s?ref=main"
-}
-/*module "postgres" {
-  
 }*/
+module "postgres" {
+    cluster-name = "patroni-test"
+    gcloud_region = "europe-west10"
+    gcloud_project = "gp-dssi"
+    gcloud_machine_type_main = "e2-standard-4"
+    gcloud_machine_type_etcd = "e2-standard-2"
+    source = "git::https://github.com/gespel/gprefarchinfra.git//tf/postgres?ref=main"
+}
