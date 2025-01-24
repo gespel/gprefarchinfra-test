111ee50f2d3a951363f12b4c1e6032d337d23aa2
gespel <sten.heimbrodt@hotmail.de>
2025-01-24 16:45:40 +0100

    feierabend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,13 @@
-/*module "k8s" {
-  source = "git::https://github.com/gespel/gprefarchinfra.git//tf/k8s?ref=main"
-}*/
-module "postgres" {
+module "k8s" {
+  cluster-name = "psi-test-cluster"
+  gcloud_region = "europe-west10"
+  gcloud_project = "gp-dssi"
+  gcloud_machine_type_main = "e2-standard-4"
+  gcloud_zones = ["europe-west10-a"]
+  gcloud_node_count = 3
+  source = "git::https://github.com/gespel/gprefarchinfra.git//terraform/k8s?ref=main"
+}
+/*module "postgres" {
     cluster-name = "patroni-test"
     gcloud_region = "europe-west10"
     gcloud_project = "gp-dssi"
@@ -9,4 +15,4 @@ module "postgres" {
     gcloud_machine_type_etcd = "e2-standard-2"
     gcloud_zones = ["europe-west10-a", "europe-west10-a", "europe-west10-a"]
     source = "git::https://github.com/gespel/gprefarchinfra.git//terraform/postgres?ref=main"
-}
+}*/
